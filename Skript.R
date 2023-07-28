b40dd217ed3e2693c953a81f9c8df7f50ca4a0ff
Mario Reutter <mario.reutter@gmail.com>
2023-07-28 12:26:36 +0200

    analysis & plots until contra ipsi

--- Skript.R
+++ Skript.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(afex)
 library(Hmisc)
+library(corrr)
 library(psych)
 library(fuzzyjoin)
 library(lme4)
@@ -13,6 +14,25 @@ exclude = c(107, #too many triggers in block 1, no triggers in block 2
 )
 
 se = function(x, na.rm = TRUE) { sd(x, na.rm) / sqrt(if(!na.rm) length(x) else sum(!is.na(x))) }
+
+dodge.width = .6 #good for factors on x-axis
+dodge = position_dodge(width=dodge.width)
+myGgTheme = theme_bw() + theme(
+  #aspect.ratio = 1,
+  plot.title = element_text(hjust = 0.5),
+  panel.background = element_rect(fill="white", color="white"),
+  legend.background = element_rect(fill="white", color="grey"),
+  legend.key=element_rect(fill='white'),
+  axis.text = element_text(color="black"),
+  axis.ticks.x = element_line(color="black"),
+  axis.line.x = element_line(color="black"),
+  axis.line.y = element_line(color="black"),
+  legend.text = element_text(size=14, color="black"),
+  legend.title = element_text(size=14, color="black"),
+  strip.text.x = element_text(size=12, color="black"),
+  axis.text.x = element_text(size=16, color="black"),
+  axis.text.y = element_text(size=16, color="black"),
+  axis.title = element_text(size=16, color="black"))
 
 
 # Valid Trials EEG --------------------------------------------------------
@@ -79,7 +99,7 @@ data_stages %>% pivot_longer(valid_NA:valid_AN, names_to="distractors", values_t
   facet_grid(rows=vars(distractors), cols=vars(block), labeller="label_both")+
   geom_vline(xintercept = 25, linetype="dashed", color="red") +
   geom_histogram(breaks=seq.int(0, 80, 5)) +
-  scale_fill_viridis_d() + theme_bw()
+  scale_fill_viridis_d() + myGgTheme
 
 #valid trials for included subjects
 data_stages %>% filter(include) %>% group_by(block) %>% 
@@ -93,7 +113,7 @@ data_stages %>%
   ggplot(aes(x=valid_AN, y=valid_NA, color=include, group=subject)) +
   geom_vline(xintercept = 25-.5, linetype="dashed", color="red") +
   geom_hline(yintercept = 25-.5, linetype="dashed", color="red") +
-  geom_line() + geom_point(size=2) + theme_bw()
+  geom_line() + geom_point(size=2) + myGgTheme
 
 
 # Data --------------------------------------------------------------------
@@ -183,21 +203,20 @@ conditions = behavioral %>% select(Vp, Condition)
 
 
 # Analysis RTs -----------------------------------------------------------
-
-
 PreStud_FINAL = PreStud_FINAL %>% 
   select(TraitPow, subject_2)
 
 behavioral_2 <- left_join(behavioral, PreStud_FINAL, by = join_by("Vp" == "subject_2"))
 
-behav_analys = behavioral_2 %>%
+RT.analysis = behavioral_2 %>%
   select(Vp, Condition, RT_congruent_Block1, RT_incongruent_Block1,
          RT_congruent_Block2, RT_incongruent_Block2, TraitPow) %>% 
   pivot_longer(-c("Vp", "Condition", "TraitPow"), values_to="Latencies") %>% 
   separate(name, into=c("RT", "TrialType", "Block")) %>% select(-RT) %>% 
-  na.omit()
+  na.omit() %>% 
+  mutate(Latencies = Latencies * 1000, #readability in plots
+         Block = ifelse(Block=="Block1", "Baseline", "Manipulation") %>% factor(levels=c("Baseline", "Manipulation"))) 
 
-RT.analysis = behav_analys
 RT.analysis %>% mutate(TraitPow = scale(TraitPow)[,1]) %>% afex::aov_ez(id="Vp", dv="Latencies",
                                                                         covariate = c("TraitPow"),
                                                                         factorize = FALSE,
@@ -214,23 +233,31 @@ RT.analysis %>% filter(Vp %in% c(134, 155, 158, 224, 239, 293, 310)) # for debug
 # 310 --> nur Block 1 vorhanden; Block 2 behav raw data file is missing
 
 #plots
+#block x condition
 RT.analysis %>% group_by(Block, Condition, Vp) %>% summarise(Latencies = mean(Latencies)) %>% #one value per participant => correct calculation of standard error
   summarise(Latencies.se = se(Latencies), Latencies = mean(Latencies)) %>% 
-  ggplot(aes(x = Block, y = Latencies, shape=Condition, group=Condition)) +
-  geom_errorbar(aes(ymin=Latencies-Latencies.se, ymax=Latencies+Latencies.se)) +
-  geom_line() + geom_point() + theme_bw()
+  ggplot(aes(x = Block, y = Latencies, color=Condition, group=Condition)) +
+  geom_errorbar(aes(ymin=Latencies-Latencies.se, ymax=Latencies+Latencies.se), size=1.5, position=dodge, width=dodge.width) +
+  geom_line(size=1.5, position=dodge) + geom_point(size=3, position=dodge) + 
+  ylab("Latency (ms)") + myGgTheme
+#ggsave("plots/RT - Block x Condition.png", dpi=300, type="cairo", width=16, height=7, units="cm", scale=1)
 
-RT.analysis %>% group_by(Block, TrialType, Condition) %>% 
-  summarise(Latencies.se = se(Latencies), Latencies = mean(Latencies)) %>% 
-  ggplot(aes(x = Block, y = Latencies, color=TrialType, shape=Condition, group=interaction(TrialType, Condition))) +
-  geom_errorbar(aes(ymin=Latencies-Latencies.se, ymax=Latencies+Latencies.se)) +
-  geom_line() + geom_point() + theme_bw()
+#trial type / congruency effect
+RT.analysis %>% group_by(TrialType, Vp) %>% summarise(Latencies = mean(Latencies)) %>% 
+  summarise(Latencies.m = mean(Latencies), Latencies.se = se(Latencies))
 
 
+#block x condition x trial type
+# RT.analysis %>% group_by(Block, TrialType, Condition) %>% 
+#   summarise(Latencies.se = se(Latencies), Latencies = mean(Latencies)) %>% 
+#   ggplot(aes(x = Block, y = Latencies, color=TrialType, shape=Condition, group=interaction(TrialType, Condition))) +
+#   geom_errorbar(aes(ymin=Latencies-Latencies.se, ymax=Latencies+Latencies.se)) +
+#   geom_line() + geom_point() + myGgTheme
 
-# RT.analysis_mlm <-lmer(Latencies ~ Condition * TrialType *  TraitPow * Block + (1|Vp), # (1|Vp) creates random intercept; (1 + Block | Vp) creates random random intercept + random slope
-# data = behav_analys)
 
+#LMM
+# RT.analysis_mlm <-lmer(Latencies ~ Condition * TrialType *  TraitPow * Block + (1|Vp), # (1|Vp) creates random intercept; (1 + Block | Vp) creates random intercept + random slope
+# data = RT.analysis)
 
 # summary(RT.analysis_mlm)
 # anova(RT.analysis_mlm)
@@ -243,9 +270,6 @@ RT.analysis %>% group_by(Block, TrialType, Condition) %>%
 # Trait Power
 
 # Main effect trait power on n2pc
-
-
-
 n2pc = dataEeg %>% select(Vp, Block, contains("Diff_P78")) %>% 
   mutate(Diff_P78_all = (Diff_P78_NA + Diff_P78_AN)/2,
          Diff_P78_all_Odd = (Diff_P78_NA_Odd + Diff_P78_AN_Odd)/2,
@@ -254,16 +278,25 @@ n2pc = dataEeg %>% select(Vp, Block, contains("Diff_P78")) %>%
 
 n2pc_TraitPow <- left_join(n2pc, PreStud_FINAL, by = join_by("Vp" == "subject_2"))
 
+#hypothesis
+with(n2pc_TraitPow, cor.test(TraitPow, Diff_P78_all, method=c("pearson"))) %>% apa::cor_apa(r_ci=T)
+
+#exploration
+n2pc_TraitPow %>%
+  select(-Vp, -Block) %>% 
+  correlate(method = "pearson", quiet=T) %>% 
+  #shave() %>% rplot(print_cor = TRUE) #plot output (not so good)
+  #network_plot() #interesting network plot
+  shave() %>% slice(-1) %>% select(-last_col()) %>% fashion() #console output
+
+#p values
 n2pc_TraitPow %>%
   select(-Vp, -Block) %>% 
   as.matrix() %>%
-  rcorr(type = "pearson")
-
-# cor.test(n2pc_TraitPow$TraitPow, n2pc_TraitPow$Diff_P78_NA, method=c("pearson"))
-
-# Interaction trait power + power condition on n2pc
+  rcorr(type = "pearson") %>% .[["P"]] %>% data.frame() %>% slice(-1) %>% select(-last_col())
 
 
+# Interaction trait power + state power condition on n2pc
 n2pc = dataEeg %>% select(Vp, Block, contains("Diff_P78")) %>% #all participants without trial excl criteria
   mutate(Diff_P78_all = (Diff_P78_NA + Diff_P78_AN)/2,
          Diff_P78_all_Odd = (Diff_P78_NA_Odd + Diff_P78_AN_Odd)/2,
@@ -271,7 +304,7 @@ n2pc = dataEeg %>% select(Vp, Block, contains("Diff_P78")) %>% #all participants
 
 PreStud_FINAL_lim <- PreStud_FINAL %>% select(c(TraitPow, subject_2))
 
-n2pc_TraitPow <- left_join(n2pc, PreStud_FINAL_lim, by = join_by("Vp" == "subject_2"), relationship = "many-to-many") %>% 
+n2pc_TraitStatePow <- left_join(n2pc, PreStud_FINAL_lim, by = join_by("Vp" == "subject_2"), relationship = "many-to-many") %>% 
   pivot_longer(-c("Vp", "Block", "TraitPow"), values_to="n2pc") %>% 
   separate(name, into=c("diff", "topo", "distractors", "subset")) %>% mutate(subset = ifelse(subset %>% is.na(), "all", subset)) %>% select(-diff, -topo) %>% 
   left_join(conditions, by="Vp")
@@ -280,14 +313,18 @@ n2pc_TraitPow <- left_join(n2pc, PreStud_FINAL_lim, by = join_by("Vp" == "subjec
 #  filter(!is.na(TraitPow))
 
 #ANOVA
-n2pc.analysis = n2pc_TraitPow %>% filter(subset == "all", distractors != "all", Vp %in% include)
+n2pc.analysis = n2pc_TraitStatePow %>% filter(subset == "all", distractors != "all", Vp %in% include) %>% 
+  mutate(Block = ifelse(Block==1, "Baseline", "Manipulation") %>% factor(levels=c("Baseline", "Manipulation")))
 n2pc.analysis %>% mutate(TraitPow = scale(TraitPow)[,1]) %>% afex::aov_ez(id="Vp", dv="n2pc",
                                                                           covariate = c("TraitPow"),
                                                                           factorize = FALSE,
                                                                           within=c("Block", "distractors"),
                                                                           between="Condition") # %>% apa::anova_apa(force_sph_corr=T)
+#effect of trait power reduced compared to simple correlation
+# => explained by other predictors and/or weaker in Block2
 
 #ANOVA
+#TODO doesn't work at Mario's PC
 n2pc.analysis %>% filter(Vp %in% c(73, 121, 142, 145, 199) == F) %>% # Analysis that shows intercept
   mutate(TraitPow = scale(TraitPow)[,1]) %>% ez::ezANOVA(wid=.(Vp), dv=.(n2pc),
                                                          within=.(Block, distractors),
@@ -296,19 +333,29 @@ n2pc.analysis %>% filter(Vp %in% c(73, 121, 142, 145, 199) == F) %>% # Analysis 
                                                          detailed = T, type = 2)  %>% apa::anova_apa(force_sph_corr=T)
 
 #plots
+#Block x Condition
 n2pc.analysis %>% group_by(Block, Condition, Vp) %>% summarise(n2pc = mean(n2pc)) %>% #one value per participant => correct calculation of standard error
   summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
-  ggplot(aes(x = Block, y = n2pc, shape=Condition, group=Condition)) +
-  geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
-  geom_line() + geom_point() + theme_bw()
-n2pc.analysis %>% group_by(Block, distractors, Condition) %>% 
-  summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
-  ggplot(aes(x = Block, y = n2pc, color=distractors, shape=Condition, group=interaction(distractors, Condition))) +
-  geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
-  geom_line() + geom_point() + theme_bw()
+  ggplot(aes(x = Block, y = n2pc, color=Condition, group=Condition)) +
+  geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se), size=1.5, position=dodge, width=dodge.width) +
+  geom_line(size=1.5, position=dodge) + geom_point(size=3, position=dodge) + 
+  ylab("N2pc (µV)") + myGgTheme
+#ggsave("plots/N2pc - Block x Condition.png", dpi=300, type="cairo", width=16, height=7, units="cm", scale=1)
+
+#Distractors
+n2pc.analysis %>% group_by(distractors, Vp) %>% summarise(n2pc = mean(n2pc)) %>% 
+  summarise(n2pc.m = mean(n2pc), n2pc.se = se(n2pc))
+
+#Block x Condition x Distractors
+# n2pc.analysis %>% group_by(Block, distractors, Condition) %>% 
+#   summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
+#   ggplot(aes(x = Block, y = n2pc, color=distractors, shape=Condition, group=interaction(distractors, Condition))) +
+#   geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
+#   geom_line() + geom_point() + myGgTheme
 
 
-# n2pc.analysis_Trait = n2pc_TraitPow %>% filter(subset == "all", distractors != "all", Vp %in% include) %>% select(-subset)
+#LMM
+# n2pc.analysis_Trait = n2pc_TraitStatePow %>% filter(subset == "all", distractors != "all", Vp %in% include) %>% select(-subset)
 # n2pc.analysis_mlm <-lmer(n2pc ~ Condition * Block * distractors * TraitPow + (1|Vp), # (1|Vp) creates random intercept; (1 + Block | Vp) creates random random intercept + random slope
 #                        data = n2pc.analysis_Trait)
 # 
@@ -319,32 +366,33 @@ n2pc.analysis %>% group_by(Block, distractors, Condition) %>%
 # tab_model(n2pc.analysis_mlm)
 
 
-#dataEeg %>% pull(Vp) %>% setdiff(conditions %>% pull(Vp))
-n2pc = dataEeg %>% select(Vp, Block, contains("Diff_P78")) %>% 
-  mutate(Diff_P78_all = (Diff_P78_NA + Diff_P78_AN)/2,
-         Diff_P78_all_Odd = (Diff_P78_NA_Odd + Diff_P78_AN_Odd)/2,
-         Diff_P78_all_Even = (Diff_P78_NA_Even + Diff_P78_AN_Even)/2) %>% 
-  pivot_longer(-c("Vp", "Block"), values_to="n2pc") %>% 
-  separate(name, into=c("diff", "topo", "distractors", "subset")) %>% mutate(subset = ifelse(subset %>% is.na(), "all", subset)) %>% select(-diff, -topo) %>% 
-  left_join(conditions, by="Vp")
-
-#ANOVA
-n2pc.analysis = n2pc %>% filter(subset == "all", distractors != "all", Vp %in% include)
-n2pc.analysis %>% afex::aov_ez(id="Vp", dv="n2pc",
-                               within=c("Block", "distractors"),
-                               between="Condition") %>% apa::anova_apa(force_sph_corr=T)
-
-#plots
-n2pc.analysis %>% group_by(Block, Condition, Vp) %>% summarise(n2pc = mean(n2pc)) %>% #one value per participant => correct calculation of standard error
-  summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
-  ggplot(aes(x = Block, y = n2pc, shape=Condition, group=Condition)) +
-  geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
-  geom_line() + geom_point() + theme_bw()
-n2pc.analysis %>% group_by(Block, distractors, Condition) %>% 
-  summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
-  ggplot(aes(x = Block, y = n2pc, color=distractors, shape=Condition, group=interaction(distractors, Condition))) +
-  geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
-  geom_line() + geom_point() + theme_bw()
+#Analysis no Trait Pow
+# #dataEeg %>% pull(Vp) %>% setdiff(conditions %>% pull(Vp))
+# n2pc = dataEeg %>% select(Vp, Block, contains("Diff_P78")) %>% 
+#   mutate(Diff_P78_all = (Diff_P78_NA + Diff_P78_AN)/2,
+#          Diff_P78_all_Odd = (Diff_P78_NA_Odd + Diff_P78_AN_Odd)/2,
+#          Diff_P78_all_Even = (Diff_P78_NA_Even + Diff_P78_AN_Even)/2) %>% 
+#   pivot_longer(-c("Vp", "Block"), values_to="n2pc") %>% 
+#   separate(name, into=c("diff", "topo", "distractors", "subset")) %>% mutate(subset = ifelse(subset %>% is.na(), "all", subset)) %>% select(-diff, -topo) %>% 
+#   left_join(conditions, by="Vp")
+# 
+# #ANOVA
+# n2pc.analysis = n2pc %>% filter(subset == "all", distractors != "all", Vp %in% include)
+# n2pc.analysis %>% afex::aov_ez(id="Vp", dv="n2pc",
+#                                within=c("Block", "distractors"),
+#                                between="Condition") %>% apa::anova_apa(force_sph_corr=T)
+# 
+# #plots
+# n2pc.analysis %>% group_by(Block, Condition, Vp) %>% summarise(n2pc = mean(n2pc)) %>% #one value per participant => correct calculation of standard error
+#   summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
+#   ggplot(aes(x = Block, y = n2pc, shape=Condition, group=Condition)) +
+#   geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
+#   geom_line() + geom_point() + myGgTheme
+# n2pc.analysis %>% group_by(Block, distractors, Condition) %>% 
+#   summarise(n2pc.se = se(n2pc), n2pc = mean(n2pc)) %>% 
+#   ggplot(aes(x = Block, y = n2pc, color=distractors, shape=Condition, group=interaction(distractors, Condition))) +
+#   geom_errorbar(aes(ymin=n2pc-n2pc.se, ymax=n2pc+n2pc.se)) +
+#   geom_line() + geom_point() + myGgTheme
 
 
 # Analysis contra ipsi ----------------------------------------------------
@@ -368,14 +416,14 @@ contraipsi.analysis %>% group_by(emotion, Vp) %>% summarise(voltage = mean(volta
   summarise(voltage.se = se(voltage), voltage = mean(voltage)) %>% 
   ggplot(aes(x = emotion, y = voltage)) +
   geom_errorbar(aes(ymin=voltage-voltage.se, ymax=voltage+voltage.se)) +
-  geom_point() + theme_bw()
+  geom_point() + myGgTheme
 
 # emotion x distractors (hemisphere main effect phrased complicatedly :D, see below)
 contraipsi.analysis %>% group_by(emotion, distractors, Vp) %>% summarise(voltage = mean(voltage)) %>% #one value per participant => correct calculation of standard error
   summarise(voltage.se = se(voltage), voltage = mean(voltage)) %>% 
   ggplot(aes(x = emotion, y = voltage, color=distractors, group=distractors)) +
   geom_errorbar(aes(ymin=voltage-voltage.se, ymax=voltage+voltage.se)) +
-  geom_line() + geom_point() + theme_bw()
+  geom_line() + geom_point() + myGgTheme
 
 
 
@@ -399,7 +447,7 @@ electrodes.analysis %>% group_by(hemisphere, Vp) %>% summarise(voltage = mean(vo
   summarise(voltage.se = se(voltage), voltage = mean(voltage)) %>% 
   ggplot(aes(x = hemisphere, y = voltage)) +
   geom_errorbar(aes(ymin=voltage-voltage.se, ymax=voltage+voltage.se)) +
-  geom_point() + theme_bw()
+  geom_point() + myGgTheme
 
 # hemisphere x distractors (N2pc effect phrased complicatedly :D)
 electrodes.analysis %>% group_by(hemisphere, distractors, Vp) %>% summarise(voltage = mean(voltage)) %>% #one value per participant => correct calculation of standard error
@@ -407,7 +455,7 @@ electrodes.analysis %>% group_by(hemisphere, distractors, Vp) %>% summarise(volt
   mutate(emotion = ifelse(hemisphere=="P7" & distractors=="NA" | (hemisphere=="P8" & distractors=="AN"), "angry", "neutral")) %>% 
   ggplot(aes(x = hemisphere, y = voltage, color=emotion, shape=distractors, group=distractors)) +
   geom_errorbar(aes(ymin=voltage-voltage.se, ymax=voltage+voltage.se)) +
-  geom_line(color="black") + geom_point() + theme_bw()
+  geom_line(color="black") + geom_point() + myGgTheme
 
 
 
