8ae13dfee96cb1d50309547214d07acc41aeae6d
Mario Reutter <mario.reutter@gmail.com>
2023-08-15 15:06:44 +0200

    unique variable names for block 1 vs 2

--- Grand Averages Robin_Mario/N2pc grand average Robin_Mario.R
+++ Grand Averages Robin_Mario/N2pc grand average Robin_Mario.R
@@ -54,22 +54,22 @@ n2pc_1_diss %>% ggplot(aes(x=Time, y=Voltage, color=ERP, group=ERP)) +
 
 
 # N2pc Block 2
-path.ga_1 = "N2pc_Block2_50Segm_only_compl_NA_AN_all_electrodes_modif.dat" %>% paste0("Generic Exports Block 1 and 2/", .)
-n2pc_1_diss_wide = read.delim(path.ga_1, sep=" ", header = F) %>% t() %>% row_to_names(row_number = 1) %>% as.data.frame
-rownames(n2pc_1_diss_wide) <-c(1:400)
+path.ga_2 = "N2pc_Block2_50Segm_only_compl_NA_AN_all_electrodes_modif.dat" %>% paste0("Generic Exports Block 1 and 2/", .)
+n2pc_2_diss_wide = read.delim(path.ga_2, sep=" ", header = F) %>% t() %>% row_to_names(row_number = 1) %>% as.data.frame
+rownames(n2pc_2_diss_wide) <-c(1:400)
 vec <- c(-99:300) 
-n2pc_1_diss_wide <- cbind(n2pc_1_diss_wide, Time = vec) #besser mit mutate(Time = vec) oder ganz fancy mutate(Time = 1:n() - 100)
-n2pc_1_diss_wide <- n2pc_1_diss_wide[,c(11,15)] #besser mit select :)
+n2pc_2_diss_wide <- cbind(n2pc_2_diss_wide, Time = vec) #besser mit mutate(Time = vec) oder ganz fancy mutate(Time = 1:n() - 100)
+n2pc_2_diss_wide <- n2pc_2_diss_wide[,c(11,15)] #besser mit select :)
 
-n2pc_1_diss = n2pc_1_diss_wide %>% gather("ERP", "Voltage", 1)
-n2pc_1_diss$ERP <- as.factor(n2pc_1_diss$ERP) 
-n2pc_1_diss$Voltage <- as.numeric(n2pc_1_diss$Voltage) 
+n2pc_2_diss = n2pc_2_diss_wide %>% gather("ERP", "Voltage", 1)
+n2pc_2_diss$ERP <- as.factor(n2pc_2_diss$ERP) 
+n2pc_2_diss$Voltage <- as.numeric(n2pc_2_diss$Voltage) 
 
-n2pc_1_diss = n2pc_1_diss %>% mutate(ERP = ifelse(ERP %>% grepl("P78", ., fixed=T), "P78", "PO78")) #better readability of legend
+n2pc_2_diss = n2pc_2_diss %>% mutate(ERP = ifelse(ERP %>% grepl("P78", ., fixed=T), "P78", "PO78")) #better readability of legend
 
 
 # N2pc Block 2 plot -------------------------------------------------------------
-n2pc_1_diss %>% ggplot(aes(x=Time, y=Voltage, color=ERP, group=ERP)) + 
+n2pc_2_diss %>% ggplot(aes(x=Time, y=Voltage, color=ERP, group=ERP)) + 
   geom_rect(xmin=180, xmax=300, ymin=-Inf, ymax=Inf, fill="grey75", colour="white", show.legend=F) + #colour sets colour of border! Use NA to remove it
   #geom_rect(xmin=300, xmax=400, ymin=-Inf, ymax=Inf, fill="grey85", colour="white", show.legend=F) +
   geom_hline(aes(yintercept=0)) + geom_vline(aes(xintercept=0), linetype=2) +
@@ -89,4 +89,8 @@ n2pc_1_diss %>% ggplot(aes(x=Time, y=Voltage, color=ERP, group=ERP)) +
         legend.justification = c(0, 1),
         legend.box.margin=margin(rep(15, times=4)))
 
-#ggsave(path.ga_1 %>% gsub(".csv", ".png", .)) #TODO add additional arguments from other script
+#ggsave(path.ga_2 %>% gsub(".csv", ".png", .)) #TODO add additional arguments from other script
+
+
+# Both blocks -------------------------------------------------------------
+
