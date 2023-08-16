bb0885513fe06b2c16eb3706af5a25d82b2bdf1b
Mario Reutter <mario.reutter@gmail.com>
2023-08-16 13:30:40 +0200

    naming topoplots, cowplot with all topos

--- Topographies/Script_Topographies.R
+++ Topographies/Script_Topographies.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(haven)
 library(janitor)
 library(dplyr)
+library(cowplot)
 
 # source("https://neuroconductor.org/neurocLite.R")
 # neuro_install("PACKAGE")
@@ -35,17 +36,17 @@ eeg.map_Block1 = eeg.map_Block1 %>% mutate(electrode = case_when(electrode == "C
 eeg.map_Block1.m = eeg.map_Block1 %>% summarise(amplitude = mean(amplitude), .by=contains("electrode"))
 eeg.map_Block1.m = eeg.map_Block1.m %>% mutate(electrodeNum = electrodeNum+1, electrode=paste0(electrodeChar, electrodeNum)) %>% 
   bind_rows(eeg.map_Block1.m) %>% arrange(electrode)
-eegUtils::topoplot(eeg.map_Block1.m, 
-                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
-                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+topo.n2pc_1 = eegUtils::topoplot(eeg.map_Block1.m, 
+                                 #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                                 palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
 
 #Conditions separate (AN vs. NA)
 eeg.map_Block1.AnNa = eeg.map_Block1 %>% 
-  mutate(electrodeNum = electrodeNum + ifelse(condition=="NA", 0, 1),
+  mutate(electrodeNum = electrodeNum + ifelse(condition=="NA", 0, 1), #NA left, i.e., angry processed left
          electrode = paste0(electrodeChar, electrodeNum))
-eegUtils::topoplot(eeg.map_Block1.AnNa, 
-                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
-                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+topo.n2pc_1.AnNa = eegUtils::topoplot(eeg.map_Block1.AnNa, 
+                                      #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                                      palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
 
 
 #Block 2
@@ -70,29 +71,46 @@ eeg.map_Block2 = eeg.map_Block2 %>% mutate(electrode = case_when(electrode == "C
 eeg.map_Block2.m = eeg.map_Block2 %>% summarise(amplitude = mean(amplitude), .by=contains("electrode"))
 eeg.map_Block2.m = eeg.map_Block2.m %>% mutate(electrodeNum = electrodeNum+1, electrode=paste0(electrodeChar, electrodeNum)) %>% 
   bind_rows(eeg.map_Block2.m) %>% arrange(electrode)
-eegUtils::topoplot(eeg.map_Block2.m, 
-                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
-                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+topo.n2pc_2 = eegUtils::topoplot(eeg.map_Block2.m, 
+                                 #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                                 palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
 
 #Conditions separate (AN vs. NA)
 eeg.map_Block2.AnNa = eeg.map_Block2 %>% 
-  mutate(electrodeNum = electrodeNum + ifelse(condition=="NA", 0, 1),
+  mutate(electrodeNum = electrodeNum + ifelse(condition=="NA", 0, 1), #NA left, i.e., angry processed left
          electrode = paste0(electrodeChar, electrodeNum))
-eegUtils::topoplot(eeg.map_Block2.AnNa, 
-                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
-                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+topo.n2pc_2.AnNa = eegUtils::topoplot(eeg.map_Block2.AnNa, 
+                                      #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                                      palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
 
 ####################################################
 # Both Blocks
 
-eeg.map_both = eeg.map_Block1.m %>% mutate(Block=1) %>% bind_rows(eeg.map_Block2.m %>% mutate(Block=2))
-eeg.map_both_FINAL = eeg.map_both %>% summarise(amplitude = mean(amplitude), .by = c("electrode", "electrodeChar", "electrodeNum")) %>% 
-  mutate(Block="Average") %>% bind_rows(eeg.map_both_FINAL) %>% mutate(Block = Block %>% as_factor())
 #N2pc
-eegUtils::topoplot(eeg.map_both_FINAL, 
-                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
-                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+eeg.map_both = eeg.map_Block1.m %>% mutate(Block="1") %>% bind_rows(eeg.map_Block2.m %>% mutate(Block="2")) #block must be char for binding with "Average"
+eeg.map_both = eeg.map_both %>% summarise(amplitude = mean(amplitude), .by = c("electrode", "electrodeChar", "electrodeNum")) %>% 
+  mutate(Block="Average") %>% bind_rows(eeg.map_both) %>% mutate(Block = Block %>% as_factor())
+
+topo.n2pc = eegUtils::topoplot(eeg.map_both %>% filter(Block=="Average"),
+                               #eeg.map_both, groups="Block", #doesn't work :(
+                               #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                               palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
 
 
+#Conditions separate (AN vs. NA)
+eeg.map_both.AnNa = eeg.map_Block1.AnNa %>% mutate(Block="1") %>% bind_rows(eeg.map_Block2.AnNa %>% mutate(Block="2")) #block must be char for binding with "Average"
+eeg.map_both.AnNa = eeg.map_both.AnNa %>% summarise(amplitude = mean(amplitude), .by = c("electrode", "electrodeChar", "electrodeNum")) %>% 
+  mutate(Block="Average") %>% bind_rows(eeg.map_both.AnNa) %>% mutate(Block = Block %>% as_factor())
 
 
+topo.n2pc.AnNa = eegUtils::topoplot(eeg.map_both.AnNa %>% filter(Block=="Average"),
+                                    #eeg.map_both, groups="Block", #doesn't work :(
+                                    #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                                    palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+
+#rows = Block (Average, 1, 2); cols = N2pc vs. Subcomponents (NA vs. AN)
+#TODO unify ranges within N2pc plots and AnNa plots respectively for better comparison (i.e., within rows)
+topo.all = cowplot::plot_grid(topo.n2pc, topo.n2pc_1, topo.n2pc_2,
+                              topo.n2pc.AnNa, topo.n2pc_1.AnNa, topo.n2pc_2.AnNa,
+                              nrow=2, labels="AUTO")
+ggsave("plots/N2pc Topographies.png", plot=topo.all, dpi=300, type="cairo", width=16, height=7, units="cm", scale=2.5)
