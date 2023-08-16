7427a4bb8dfdb1beb2664db07a068d9027c46fcd
RobinWill92 <137183058+RobinWill92@users.noreply.github.com>
2023-08-16 11:14:46 +0200

    Created Topoplot for Block 2 and for Block 1 and 2 combined

--- Topographies/Script_Topographies.R
+++ Topographies/Script_Topographies.R
@@ -1,7 +1,6 @@
-library(remotes)
 remotes::install_github("craddm/eegUtils")
+library(remotes)
 library(eegUtils)
-
 library(tidyverse)
 library(haven)
 library(janitor)
@@ -50,4 +49,50 @@ eegUtils::topoplot(eeg.map_Block1.AnNa,
 
 
 #Block 2
-eeg.map_Block2 = read_table("Topographies/Area_Block2_all_partic_all_Channel_Diff_all.txt", na="???")
+eeg.map_Block2 = read_table("Topographies/Area_Block2_all_partic_all_Channel_Diff_all.txt", na="???") %>% 
+  select(Vp, contains("all")) %>% select(-contains("Odd")) %>% select(-contains("Even")) %>% 
+  summarise(across(-Vp, mean)) %>% #means across participants
+  pivot_longer(cols=everything(), names_to="electrode", values_to="amplitude") %>% 
+  separate(electrode, into=c("electrode", "condition"), sep="-") %>% mutate(condition = condition %>% gsub("Diff_all_", "", .)) %>% 
+  mutate(electrode = electrode %>% gsub("Contra_", "", .) %>% gsub("_Ipsi_.+", "", .))
+#eeg.map_Block2 %>% pull(electrode) %>% unique()
+eeg.map_Block2 = eeg.map_Block2 %>% mutate(electrode = case_when(electrode == "C"  ~ "C3",
+                                                                 electrode == "FT" ~ "FT9",
+                                                                 electrode == "O"  ~ "O1",
+                                                                 electrode == "T"  ~ "T7",
+                                                                 electrode == "TP" ~ "TP9",
+                                                                 electrode == "PO" ~ "PO7",
+                                                                 T ~ electrode %>% substr(1, electrode %>% sapply(nchar) - 1))) %>% 
+  mutate(electrodeChar = electrode %>% gsub("\\d", "", .), electrodeNum = electrode %>% gsub("\\D", "", .) %>% as.integer())
+#eeg.map_Block2 %>% filter(electrode %in% chanlocs$electrode == F) #missing entries
+
+#N2pc
+eeg.map_Block2.m = eeg.map_Block2 %>% summarise(amplitude = mean(amplitude), .by=contains("electrode"))
+eeg.map_Block2.m = eeg.map_Block2.m %>% mutate(electrodeNum = electrodeNum+1, electrode=paste0(electrodeChar, electrodeNum)) %>% 
+  bind_rows(eeg.map_Block2.m) %>% arrange(electrode)
+eegUtils::topoplot(eeg.map_Block2.m, 
+                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+
+#Conditions separate (AN vs. NA)
+eeg.map_Block2.AnNa = eeg.map_Block2 %>% 
+  mutate(electrodeNum = electrodeNum + ifelse(condition=="NA", 0, 1),
+         electrode = paste0(electrodeChar, electrodeNum))
+eegUtils::topoplot(eeg.map_Block2.AnNa, 
+                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+
+####################################################
+# Both Blocks
+
+eeg.map_both = eeg.map_Block1.m %>% mutate(Block=1) %>% bind_rows(eeg.map_Block2.m %>% mutate(Block=2))
+eeg.map_both_FINAL = eeg.map_both %>% summarise(amplitude = mean(amplitude), .by = c("electrode", "electrodeChar", "electrodeNum")) %>% 
+  mutate(Block="Average") %>% bind_rows(eeg.map_both_FINAL) %>% mutate(Block = Block %>% as_factor())
+#N2pc
+eegUtils::topoplot(eeg.map_both_FINAL, 
+                   #chanLocs = chanlocs, #there seems to be a default chanLocs file that recognizes electrode names in the 10-20 notation
+                   palette = "viridis", contour = FALSE, interp_limit = "head", highlights=c("P7", "P8")) #chan_marker="name"
+
+
+
+
